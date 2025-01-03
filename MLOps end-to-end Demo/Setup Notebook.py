29cb83603da61a01f3b9650c0c0086f9d6039a09
EdgarAgRod <EdgarAgRod>
2025-01-03 16:11:00 +0000

    Added DBDemos
    Added "MLOps End-to-End" from DBDemos

---
+++ MLOps end-to-end Demo/Setup Notebook.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.list_demos()
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('mlops-end2end')
